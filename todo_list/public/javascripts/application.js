a186f1f5948dc4c212ea5b813a601697a867c28d
Derek Gerson <derekgrsn@gmail.com>
2015-08-21 14:06:59 -0700

    end of day commit

--- todo_list/public/javascripts/application.js
+++ todo_list/public/javascripts/application.js
@@ -25,11 +25,38 @@ $(document).on('ready', function(){
 	})
 })
 
+var List = function() {
+	this.taskContainer = $('#list');
+}
+
+// List.prototype.request = function(urlVal, typeVal, success) {
+// 	$.ajax(
+// 		url: urlVal,
+// 		type: typeVal
+// 	).done(function(sData){
+// 		success();
+// 	})
+// };
+
+List.prototype.listAll = function() {
+	
+
+	$.ajax({
+		url: "http://localhost:3000/tasks",
+		dataType: "JSON"
+	}).done(function(sData){
+		var allTasks = sData;
+		for(var i = 0; i < allTasks.length; i++){
+			// load each task and add to page
+		}
+	})
+};
+
 var SingleTask = function(taskObject) {
 	this.body = taskObject.body;
 }
 
-git SingleTask.prototype.display = function() {
+SingleTask.prototype.display = function() {
 	var task = $("<div>").append( '<input type="checkbox">'+this.body+' <a href="/tasks"> delete </a> <br>' )
 	return task;
 };
